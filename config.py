2915e095e0b45b6e20f126088496d603df95c58d
adityabs07 <ytprimium0007@gmail.com>
2026-01-08 11:08:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "c17eb956b387ff9f0dae6c7409bc6cf5")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "adisxbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Aditya7:<658268>@cluster0.fxqlyge.mongodb.net/?appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ytimepremium_db_user:ulnjq31wqHe0JS7G@cluster0.lnrygsl.mongodb.net")
     DATABASE_NAME = environ.get("DATABASE_NAME", "adisx-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "7637180874"))
 
@@ -27,4 +27,5 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
